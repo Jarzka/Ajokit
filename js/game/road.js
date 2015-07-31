de3b66a344ccb9e4d153246560488fb6f886b206
Jari Hanhela <jarihanhela@gmail.com>
2015-07-31 08:28:56 +0300

    Implemented support for adding route connections between nodes.

--- js/game/road.js
+++ js/game/road.js
@@ -17,14 +17,13 @@ TRAFFICSIM_APP.game.Road = function (worldController, model, roadType) {
 
     this._roadType = roadType;
     this._roadNodes = [];
+    this._roadRoutes = [];
 
     function constructor() {
         TRAFFICSIM_APP.game.GameplayObject.call(self, worldController, model);
         var nodePositions = self.getNodePositionsByRoadType(self._roadType);
-        self.initializeNodes(nodePositions);
-
-        console.log("Road has " + self._roadNodes.length + " nodes:" );
-        console.log(self._roadNodes);
+        var nodeConnections = self.getNodeConnectionsByRoadType(self._roadType);
+        self.initializeNodes(nodePositions, nodeConnections);
     }
 
     constructor(worldController, model);
@@ -35,6 +34,9 @@ TRAFFICSIM_APP.game.Road.prototype = Object.create(TRAFFICSIM_APP.game.GameplayO
 TRAFFICSIM_APP.game.Road.prototype.getNodePositionsByRoadType = function (roadType) {
     /* Node position is relative to the parent object's width and height:
      * [0, 0] is the upper left corner, [1, 1] is the lower right corner, [0.5, 0.5] is the center and so on...
+     *
+     * Node's position in the array determines its "name". For example the first node in the array is node number 1,
+     * the second is node 2 etc.
      */
     if (roadType === "VERTICAL") {
         return [
@@ -62,12 +64,34 @@ TRAFFICSIM_APP.game.Road.prototype.getNodePositionsByRoadType = function (roadTy
     }
 };
 
-TRAFFICSIM_APP.game.Road.prototype.initializeNodes = function (positions) {
+
+TRAFFICSIM_APP.game.Road.prototype.getNodeConnectionsByRoadType = function (roadType) {
+    /* Connections is an array in which each element is an array of a pair of connected nodes.
+     * For example [[1, 3], [2, 4]] means that there are two connections:
+     * nodes 1 and 3 are connected and
+     * nodes 2 and 4 are connected. */
+    if (roadType === "VERTICAL") {
+        return [
+            [1, 3],
+            [2, 4]
+        ];
+    }
+};
+
+TRAFFICSIM_APP.game.Road.prototype.initializeNodes = function (positions, connections) {
     var self = this;
 
     positions.forEach(function (position) {
         var node = new TRAFFICSIM_APP.game.RoadNode(self._worldController, position);
         self._roadNodes.push(node);
+    });
+
+    connections.forEach(function (connection) {
+        var route = new TRAFFICSIM_APP.game.RoadRoute(self._worldController,
+            self._roadNodes[connection[0]],
+            self._roadNodes[connection[1]]
+        );
+        self._roadRoutes.push(route);
     });
 };
 
