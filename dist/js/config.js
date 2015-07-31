8cb63b269452bd49949257daa21c2c7cc53cc4a0
Jari Hanhela <jarihanhela@gmail.com>
2015-07-31 08:07:50 +0300

    Removed compiled code files from the repo.

--- dist/js/config.js
+++
@@ -1,4 +0,0 @@
-/*********************************************************************
- * The configurations below are meant for easy system configuration
- * without the need of rebuilding the app.
- *********************************************************************/
