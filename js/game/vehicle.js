7d9ef4c4c721d3f5499ffb43f30c9c612d296535
Jari Hanhela <jarihanhela@gmail.com>
2015-08-08 21:12:49 +0300

    Removed constructor function from vehicles (it was copied for every instance).

--- js/game/vehicle.js
+++ js/game/vehicle.js
@@ -6,7 +6,7 @@
     TRAFFICSIM_APP.game.VehicleType = {
         "CAR": 1
     };
-    
+
     TRAFFICSIM_APP.game.Vehicle = function (worldController, model, vehicleType) {
         var self = this;
 
@@ -14,11 +14,7 @@
         this._currentNode = null;
         this._currentRoute = null;
 
-        function constructor(worldController, model) {
-            TRAFFICSIM_APP.game.GameplayObject.call(self, worldController, model);
-        }
-
-        constructor(worldController, model);
+        TRAFFICSIM_APP.game.GameplayObject.call(self, worldController, model);
     };
 
     TRAFFICSIM_APP.game.Vehicle.prototype = Object.create(TRAFFICSIM_APP.game.GameplayObject.prototype);
@@ -40,26 +36,28 @@
     };
 
     TRAFFICSIM_APP.game.Vehicle.prototype.update = function(deltaTime) {
+        var self = this;
+
         if (this._currentNode) {
             logger.log(logger.LogType.DEBUG, "Car " + this._id + " reached a now, finding next route...");
             //takeNextRoute();
         }
 
         function takeNextRoute() {
-            // Randomly pick one of the routes connected to the current node (not the one that we just travelled).
-            var connections = this._currentNode.getConnectedRoutes();
+            // Randomly pick one of the routes connected to the current node (but not the one that we just drove).
+            var connections = self._currentNode.getConnectedRoutes();
 
             var nextRoute = null;
 
-            while (nextRoute == null || nextRoute == this._currentNode) {
+            while (nextRoute == null || nextRoute == self._currentNode) {
                 nextRoute = TRAFFICSIM_APP.utils.math.randomValue(0, connections.length - 1);
             }
 
-            logger.log(logger.LogType.DEBUG, "Car " + this._id + " taking next route" +
+            logger.log(logger.LogType.DEBUG, "Car " + self._id + " taking next route" +
                 "from (x:" + nextRoute.startNode.x + " y:" + nextRoute.startNode.y + ") " +
                 "to (x:" + nextRoute.endNode.x + " y:" + end.startNode.y + ")");
 
-            this._currentNode = null;
+            self._currentNode = null;
 
         }
     };
