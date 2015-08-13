5fee80b612cc00c98541435ce0ee42603d7f12cb
Jari Hanhela <jarihanhela@gmail.com>
2015-08-13 21:18:35 +0300

    Added OpenLine "enum" for traffic lights controller.

--- js/game/traffic_lights_controller.js
+++ js/game/traffic_lights_controller.js
@@ -1,6 +1,14 @@
 TRAFFICSIM_APP.game = TRAFFICSIM_APP.game || {};
 
-TRAFFICSIM_APP.game.TrafficLightsCOntroller = function (road) {
+TRAFFICSIM_APP.game.OpenLine = {
+    "TOP": 0,
+    "RIGHT": 1,
+    "DOWN": 2,
+    "BOTTOM": 3
+};
+
+
+TRAFFICSIM_APP.game.TrafficLightsController = function (road) {
     var self = this;
 
     this._road = road;
