fab67aa83726f0cda66224238d2f269d6a31538f
Jari Hanhela <jarihanhela@gmail.com>
2015-08-28 20:28:52 +0300

    Implement core code for better vehicle physics.

--- js/game/vehicle.js
+++ js/game/vehicle.js
@@ -15,9 +15,13 @@
         this._vehicleType = vehicleType;
         this._currentNode = null;
         this._currentRouteTargetNode = null;
-        this._speed = math.randomValue(5, 10);
+        this._speed = 0;
+        this._acceleratorPedal = 0; // Between 0 and 1
+        this._maxSpeed = math.randomValue(4, 7);
+        this._acceleration = math.randomValue(2, 7);
+        this._deceleration = this._acceleration;
 
-        TRAFFICSIM_APP.game.GameplayObject.call(self, worldController, model);
+            TRAFFICSIM_APP.game.GameplayObject.call(self, worldController, model);
 
         this._setCollisionMask();
     };
@@ -92,66 +96,126 @@
     TRAFFICSIM_APP.game.Vehicle.prototype.update = function (deltaTime) {
         var self = this;
 
-        if (this._currentNode) {
-            findNextRoute();
-        } else {
-            moveTowardsTargetNode();
-        }
+        handleRouteFinding();
+        handleLogicalMotion();
+        handlePhysicalMotion();
+        handleTargetReached();
 
-        function moveTowardsTargetNode() {
-            // Store current position and angle
+        function handleLogicalMotion() {
 
-            var oldPosition = new Vector3(self._position.x, self._position.y, self._position.z);
-            var oldAngle = self._angle;
+            handleAccelerationPedal();
+            handleSteeringWheel();
+            handleReleaseAccelerationPenalToStopAtNextPoint();
 
-            // Move
+            function handleAccelerationPedal() {
 
-            var angleBetweenCurrentAndTargetPoint = math.angleBetweenPointsWhenYIncreasesDown(
-                self._position.x,
-                self._position.z,
-                self._currentRouteTargetNode.position.x,
-                self._currentRouteTargetNode.position.z);
-
-            self.setPosition(new Vector3(self._position.x + Math.cos(angleBetweenCurrentAndTargetPoint) * self._speed * deltaTime,
-                    self._position.y,
-                    self._position.z - Math.sin(angleBetweenCurrentAndTargetPoint) * self._speed * deltaTime));
-
-            self.setAngle(angleBetweenCurrentAndTargetPoint);
-
-            // Check collision & rollback if on collision
-
-            if (self.onCollision()) {
-                self.setPosition(oldPosition);
-                self.setAngle(oldAngle);
+                if (self._currentRouteTargetNode && self._currentRoute) {
+                    self._acceleratorPedal = 1;
+                } else {
+                    self._acceleratorPedal = 0;
+                }
             }
 
-            // Target reached
+            function handleSteeringWheel() {
+                // Currently there is no "real" steering wheel, we just hardly rotate the car to the next target point.
+                if (self._currentRouteTargetNode) {
+                    var angleBetweenCurrentAndTargetPoint = math.angleBetweenPointsWhenYIncreasesDown(
+                        self._position.x,
+                        self._position.z,
+                        self._currentRouteTargetNode.position.x,
+                        self._currentRouteTargetNode.position.z);
+                    self.setPosition(new Vector3(self._position.x + Math.cos(angleBetweenCurrentAndTargetPoint) * self._speed * deltaTime,
+                        self._position.y,
+                        self._position.z - Math.sin(angleBetweenCurrentAndTargetPoint) * self._speed * deltaTime));
+                    self.setAngle(angleBetweenCurrentAndTargetPoint);
+                }
 
+            }
+
+            function handleReleaseAccelerationPenalToStopAtNextPoint() {
+                // TODO
+            }
+
+        }
+
+        function handlePhysicalMotion() {
+            handleAcceleration();
+            handleDeceleration();
+            handleSpeed();
+
+            function handleAcceleration() {
+                if (self._acceleratorPedal > 0) {
+                    self._speed += self._acceleration * deltaTime;
+
+                    if (self._speed > self._maxSpeed) {
+                        self._speed = self._maxSpeed;
+                    }
+                }
+            }
+
+            function handleDeceleration() {
+                if (self._acceleratorPedal == 0) {
+                    self._speed -= self._deceleration * deltaTime;
+
+                    if (self._speed < 0) {
+                        self._speed = 0;
+                    }
+                }
+            }
+
+            function handleSpeed() {
+                // Store current position and angle
+                var oldPosition = new Vector3(self._position.x, self._position.y, self._position.z);
+                var oldAngle = self._angle;
+
+                // Move towards next target point
+                self.setPosition(new Vector3(self._position.x + Math.cos(self._angle) * self._speed * deltaTime,
+                    self._position.y,
+                    self._position.z - Math.sin(self._angle) * self._speed * deltaTime));
+
+                // Check collision & rollback if on collision
+
+                if (self.onCollision()) {
+                    self.setPosition(oldPosition);
+                    self.setAngle(oldAngle);
+                }
+            }
+        }
+
+        function handleTargetReached() {
             if (isDestinationReached()) {
                 self._currentNode = self._currentRouteTargetNode;
+                self._currentRoute = null;
+                self._currentRouteTargetNode = null;
+            }
+
+            function isDestinationReached() {
+                if (self._currentRouteTargetNode) {
+                    return math.distance(
+                            self._position.x,
+                            self._position.y,
+                            self._position.z,
+                            self._currentRouteTargetNode.position.x,
+                            self._currentRouteTargetNode.position.y,
+                            self._currentRouteTargetNode.position.z) <= 0.2;
+                }
+
+                return false;
             }
         }
 
-        function isDestinationReached() {
-            return math.distance(
-                    self._position.x,
-                    self._position.y,
-                    self._position.z,
-                    self._currentRouteTargetNode.position.x,
-                    self._currentRouteTargetNode.position.y,
-                    self._currentRouteTargetNode.position.z) <= 0.2;
-        }
+        function handleRouteFinding() {
+            if (self._currentNode) { // Try to find the next target node
+                // Randomly pick one of the routes connected to the current node (but not the one that we just drove).
+                var freeStartingConnections = self._currentNode.getConnectedFreeStartingRoutes();
 
-        function findNextRoute() {
-            // Randomly pick one of the routes connected to the current node (but not the one that we just drove).
-            var freeStartingConnections = self._currentNode.getConnectedFreeStartingRoutes();
+                if (freeStartingConnections.length > 0) {
+                    self._currentNode = null;
 
-            if (freeStartingConnections.length > 0) {
-                self._currentNode = null;
-
-                var nextRoute = freeStartingConnections[TRAFFICSIM_APP.utils.math.randomValue(0, freeStartingConnections.length - 1)];
-                self._currentRoute = nextRoute;
-                self._currentRouteTargetNode = nextRoute.endNode;
+                    var nextRoute = freeStartingConnections[TRAFFICSIM_APP.utils.math.randomValue(0, freeStartingConnections.length - 1)];
+                    self._currentRoute = nextRoute;
+                    self._currentRouteTargetNode = nextRoute.endNode;
+                }
             }
         }
 
